67720d337f7d51b87e8e0af367d401517b49f0e5
icchon <66839886+icchon@users.noreply.github.com>
2024-03-14 18:52:16 +0900

    Update settings.py

--- testproject/settings.py
+++ testproject/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_DIR = Path.joinpath(BASE_DIR, 'static')
+#STATIC_DIR = Path.joinpath(BASE_DIR, 'static')
 
 
 
@@ -121,14 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = BASE_DIR / "static"
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 STATICFILES_DIRS = [
  
