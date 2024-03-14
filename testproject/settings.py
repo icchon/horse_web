abc0d70d789c720978970e759184a5b6db42e6a6
icchon <66839886+icchon@users.noreply.github.com>
2024-03-14 22:18:58 +0900

    Update settings.py

--- testproject/settings.py
+++ testproject/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_DIR = Path.joinpath(BASE_DIR, 'static')
+#STATIC_DIR = Path.joinpath(BASE_DIR, 'static')
 
 
 
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 
 # Default primary key field type
