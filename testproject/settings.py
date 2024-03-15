19888c44c4c51ba65d0f98f1e976ab6e53880128
icchon <66839886+icchon@users.noreply.github.com>
2024-03-15 15:29:48 +0900

    Update settings.py

--- testproject/settings.py
+++ testproject/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-#STATIC_DIR = Path.joinpath(BASE_DIR, 'static')
+STATIC_DIR = Path.joinpath(BASE_DIR, 'static')
 
 
 
@@ -132,7 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 """
 STATICFILES_DIRS = [
   BASE_DIR / "static",
